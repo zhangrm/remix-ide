be10038782e69c2450eb849182e2dc3907979a1d
yann300 <yann.levreau@gmail.com>
2018-08-20 16:03:03 +0200

    don't display provider url

--- src/app/debugger/remix-debugger/src/ui/TxBrowser.js
+++ src/app/debugger/remix-debugger/src/ui/TxBrowser.js
@@ -46,19 +46,18 @@ var css = csjs`
     margin-bottom: 10px;
   }
 `
-function TxBrowser (_parent) {
+function TxBrowser (_parent, opts) {
   this.event = new EventManager()
 
   this.blockNumber
   this.txNumber
   this.view
-  this.displayConnectionSetting = true
+  this.displayConnectionSetting = opts.displayConnectionSetting
   this.basicPanel = new DropdownPanel('Transaction', {json: true})
   this.basicPanel.data = {}
   this.web3 = _parent.debugger.web3
   var self = this
   _parent.event.register('providerChanged', this, function (provider) {
-    self.displayConnectionSetting = provider === 'INTERNAL'
     self.setDefaultValues()
     if (self.view) {
       yo.update(self.view, self.render())
